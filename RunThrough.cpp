32a5011807e1c8d7dbe2795f6229f5e19e90dad8
Jiayang Li <gscsdyang@gmail.com>
2017-04-08 22:30:59 +0000

    finish run through file

--- RunThrough.cpp
+++ RunThrough.cpp
@@ -5,72 +5,66 @@ using namespace std;
 int main()
 {
     
-    generateDungeon();// basically we should have somthing generate the whole dungeon, aka have map made, assign each room lay out to each rooom, this whole thing can be under one single class tbh
+    map.generateDungeon();// basically we should have somthing generate the whole dungeon, aka have map made, assign each room lay out to each rooom, this whole thing can be under one single class tbh
     
     //first introduce the player to the game:
-    welomeMessage(); // print out put the situation of the game, the rules, the controls, the goals
+    menu.welomeMessage(); // print out put the situation of the game, the rules, the controls, the goals
     //------------
     //then out put questions regarding asking names and such and store it in Player class
     // bunch code take in in put and set up player poperly
     //------
-    printPlayerMap(); //print out the map player should see, NOT the over all, its just rooms player've discovered
-    printRoom(); // print out the room lay out player is in and player position in the room and the monster positions
+    menu.printPlayerMap(); //print out the map player should see, NOT the over all, its just rooms player've discovered
+    menu.printRoom(); // print out the room lay out player is in and player position in the room and the monster positions
     
     
     
     while (input != "QUIT"){ // it keeps running take in player action n stuch and play the game
         
         if (input == "map"){
-            printPlayerMap();
+            player.printPlayerMap();
         }
         
         if (input == "help"){
-            printInstructions();
+            menu.printInstructions();
         }
         
         if (input == any of the movements input "WASD"){
             movements shit //move the player corsponding the input
             update the player positon in the map
-            printRoom; //show updated positions after movement
+            map.printRoom; //show updated positions after movement
         }
         
-        if (player position == monster possiton && monster is still alive){
-            void fightMonster()// call method that fight monster ( it should print out monster's ACSII, printout questions, take in questions, update Player class's health/key numbers accordingly)
+        if (player.getPosition == monster.possiton && monster is still alive){
+            monster.fightMonster()// call method that fight monster ( it should print out monster's ACSII, printout questions, take in questions, update Player class's health/key numbers accordingly)
             if ( player.getHealth <= 0){
-                void printDeathMSG()
+                player.printDeathMSG();
                 END GAME
             }
             then change monster positon to dead, so if player set on it again it wouldnt triger.
         }
         
         if (player positon == door position && all monster in room is killed(this should be a method inside Room class to check.)){
-            ask player if want to move to next room at this positon (Y/N)
+            map method that ask player if want to move to next room at this positon (Y/N)
             if (input == Y){
-                update player's position accordingly to a new room, 
-                so reset the room and print out the new room lay out,
-                update the PLAYERMap, so shows what player is discovered.
+                map method update player's position accordingly to a new room, 
+                map method so reset the room and print out the new room lay out,
+                map method update the PLAYERMap, so shows what player is discovered.
             }
         }
         
         if (playerPosition == doorPositon && all monster are NOT dead){
-            print message that not all monster are killed in the room;
+            map method print message that not all monster are killed in the room;
         }
         
-        if (playerRoom == BossRoom && playerKeys)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-       
-        if (boss == killed){ // if complete the game
+        if (playerRoom == BossRoom && player.getKeys == 4 ){
+            monster.printBossQ();
+            monster.fightBoss();
+            if (monster.BossHealth == dead){
+                 monster.printWinMSG();
             END GAME
+            }
         }
+        
     }
     
 }
